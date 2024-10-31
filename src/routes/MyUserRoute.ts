86c3dda9548e22b3d1973cddcb033ce8789577df
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-10-31 15:25:14 -0300

    feat: added Update user route

--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,9 +1,17 @@
-import express from "express";
+import express, { Handler } from "express";
 import MyUserController from "../controllers/MyUserController";
-import { jwtCheck } from "../middleware/auth";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
 router.post("/", jwtCheck, MyUserController.createCurrentUser as any);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse as Handler,
+  validateMyUserRequest as any,
+  MyUserController.updateCurrentUser as any
+);
 
 export default router;
