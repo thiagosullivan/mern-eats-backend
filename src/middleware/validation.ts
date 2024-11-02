0ba6b74d44b413b87f494a0aef7e2b817a7c79a2
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-11-01 23:57:31 -0300

    feat: endpoint to createRestaurant

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -1,13 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 
-const handleValidationErrors = async (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({ errors: errors.array})
-    }
-    next();
-}
+const handleValidationErrors = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array });
+  }
+  next();
+};
 
 export const validateMyUserRequest = [
   body("name").isString().notEmpty().withMessage("Name must be a string"),
@@ -17,5 +21,34 @@ export const validateMyUserRequest = [
     .withMessage("Address must be a string"),
   body("city").isString().notEmpty().withMessage("City must be a string"),
   body("country").isString().notEmpty().withMessage("Country must be a string"),
+  handleValidationErrors,
+];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName")
+    .isString()
+    .notEmpty()
+    .withMessage("Restaurant name must be a string"),
+  body("city").isString().notEmpty().withMessage("City must be a string"),
+  body("country").isString().notEmpty().withMessage("Country must be a string"),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("Delivery price must be a positive number"),
+  body("estimatedDeliveryTime")
+    .isInt({ min: 0 })
+    .withMessage("Estimated delivery time must be a positive integar"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be an array")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisines array cannot be empty"),
+  body("menuItems")
+    .isArray()
+    .withMessage("Menu items must be an array"),
+  body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("Menu item price is required and must be a positive number"),
   handleValidationErrors,
 ];
---
+++ src/routes/MyRestaurantRoute.ts
@@ -0,0 +1,26 @@
+import express, { Handler, RequestHandler } from "express";
+import multer from "multer";
+import MyRestaurantController from "../controllers/MyRestaurantController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyRestaurantRequest } from "../middleware/validation";
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
+
+router.post(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest as any,
+  jwtCheck,
+  jwtParse as Handler,
+  MyRestaurantController.createMyRestaurant as RequestHandler
+);
+
+export default router;
